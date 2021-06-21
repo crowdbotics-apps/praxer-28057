92be6270c55493167d1c975b8edbb9b1a141b247
Crowdbotics <team@crowdbotics.com>
2021-06-21 01:37:49 +0000

    add component Push Notifications

---
+++ node_modules/react-native-onesignal/src/events.js
@@ -0,0 +1,8 @@
+// events
+export const NOTIFICATION_WILL_SHOW = 'OneSignal-notificationWillShowInForeground';
+export const NOTIFICATION_OPENED    = 'OneSignal-remoteNotificationOpened'
+export const IN_APP_MESSAGE_CLICKED = 'OneSignal-inAppMessageClicked';
+export const PERMISSION_CHANGED = 'OneSignal-permissionChanged';
+export const SUBSCRIPTION_CHANGED = 'OneSignal-subscriptionChanged';
+export const EMAIL_SUBSCRIPTION_CHANGED = 'OneSignal-emailSubscriptionChanged';
+export const SMS_SUBSCRIPTION_CHANGED = 'OneSignal-smsSubscriptionChanged';
